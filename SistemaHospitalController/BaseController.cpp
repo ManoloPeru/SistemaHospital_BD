dffc8992dadb9cb5a39da11f0dc49795b0d1c648
Manuel Chavez Castro <assetec.analista@outlook.com>
2024-11-28 16:14:24 -0500

    Conexión a la PUCP

--- SistemaHospitalController/BaseController.cpp
+++ SistemaHospitalController/BaseController.cpp
@@ -13,8 +13,10 @@ SqlConnection^ BaseController::getObjConexion() {
 
 void BaseController::abrirConexion() {
 	/*Paso 1, establecer la cadena de conexion*/
-	/*Local*/
-	this->objConexion->ConnectionString = "Server=localhost;DataBase=lpoo_2024_2;User id=sa;Password=Passw0rd";
+	/*LOCAL*/
+	//this->objConexion->ConnectionString = "Server=localhost;DataBase=lpoo_2024_2;User id=sa;Password=Passw0rd";
+	/*PUCP*/
+	this->objConexion->ConnectionString = "Server=200.16.7.140;DataBase=mchavez;User id=mchavez;Password=CsXyEPeqdXYt";
 	/*AWS*/
 	//this->objConexion->ConnectionString = "Server=lpoo2024.c1uehfrpd23g.us-east-1.rds.amazonaws.com;DataBase=lpoo2024;User id=lpoo2024;Password=Passw0rd2024#";
 	/*Paso 2, abrir la conexion*/
